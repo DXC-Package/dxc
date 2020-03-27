1a29125f4029976de0dde207b45bd56ef3b1d210
DXC AI <anjutyagi23@gmail.com>
2020-03-27 01:38:24 -0400

    updated method

--- dxc/ai.py
+++ dxc/ai.py
@@ -8,3 +8,10 @@ def convert(my_name):
     """
 
     print(f"I'll convert a notebook for you some day, {my_name}.")
+
+def read_data_frame_from_remote_json(json_url):
+    with urllib.request.urlopen(json_url) as url:
+        json_data = json.loads(url.read().decode())
+    df = flatten_json_into_dataframe(json_data)
+    return(df)
+
